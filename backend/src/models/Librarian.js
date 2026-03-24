3d363474eff1eaa9cd2397c49bd9767f367adfcb
Min Thit Tun <minthittun@gmail.com>
2026-03-24 18:30:24 +0630

    Removed Librarian

--- backend/src/models/Librarian.js
+++ backend/src/models/Librarian.js
@@ -1,10 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const librarianSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true }
-}, { timestamps: true });
+const librarianSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
-const Librarian = mongoose.model('Librarian', librarianSchema);
+const Librarian = mongoose.model("Librarian", librarianSchema);
 export default Librarian;
